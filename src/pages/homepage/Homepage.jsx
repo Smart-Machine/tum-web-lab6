657fe6580b4e85fa8bd02cfa8a70927244966d02
Smart-Machine <radu.calin500@gmail.com>
2024-05-16 21:53:32 +0300

    feature: add homepage page

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import Posts from "../../components/posts/Posts";
+import "./Homepage.css";
+
+export default function Homepage() {
+	return (
+		<>
+			<div className="home">
+				<Posts />
+			</div>
+		</>
+	);
+}
